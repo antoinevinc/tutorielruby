c00b2df1304d2ab963d51cd348d94fac8231ad9d
antoinevinc <44704438+antoinevinc@users.noreply.github.com>
2018-11-22 14:23:57 -0400

    Create exo_07_c.rb

---
+++ exo_07_c.rb
@@ -0,0 +1,7 @@
+user_name = gets.chomp
+puts user_name
+
+# les différences entre les 3 programmes sont les suivantes :
+#- programme a : Demande le nom de l'utilisateur et affiche l'entrée
+#- programme b : Demande le nom de l'utilisateur et affiche l'entrée précédé d'un ">"
+#- programme c : attends une entrée et l'affiche
