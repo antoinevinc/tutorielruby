82dcce6079e9041042a30669dba29cf8edfa4d6b
antoinevinc <44704438+antoinevinc@users.noreply.github.com>
2018-11-22 13:57:19 -0400

    Create exo_05.rb

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heures de travail à THP" # affiche la chaîne entre guillemets
+puts "Travail : #{10 * 5 * 11}" # évalue le résultat de 10 x 5 x 11
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}" # évalue le résultat de 10 x 5 x 11 x 60
+
+puts "Et en secondes ?" # affiche la chaîne entre guillemets
+
+puts 10 * 5 * 11 * 60 * 60 # affiche le résultat de 10 x 5 x 11 x 60 x 60
+
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?" # affiche la chaîne entre guillemets
+
+puts 3 + 2 < 5 - 7 # affiche false car cette équation est fausse : 3 + 2  n'est pas strictement inférieure à 5 - 7
+
+puts "Ça fait combien 3 + 2 ? #{3 + 2}" # affiche la chaîne entre guillemets suivi du résultat de l'expression 3 + 2 : 5
+puts "Ça fait combien 5 - 7 ? #{5 - 7}" # affiche la chaîne entre guillemets suivi du résultat de l'expression 5 - 7 : -2
+
+puts "Ok, c'est faux alors !" # affiche la chaîne entre guillemets
+
+puts "C'est drôle ça, faisons-en plus :" # affiche la chaîne entre guillemets
+
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}" # affiche la chaîne entre guillemets suivi de true
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" # affiche la chaîne entre guillemets suivi de true
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" # affiche la chaîne entre guillemets suivi de false
