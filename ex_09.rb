a60213b011142f88e81d8a4c9e5fd7413128e830
antoinevinc <44704438+antoinevinc@users.noreply.github.com>
2018-11-22 14:29:16 -0400

    Create ex_09.rb

---
+++ ex_09.rb
@@ -0,0 +1,5 @@
+puts "Bonjour, veuillez entrer votre prénom s'il vous plaît :"
+user_firstname = gets.chomp
+puts "Veuillez maintenant entrer votre nom s'il vous plaît :"
+user_name = gets.chomp
+puts "Bonjour, " + user_firstname + " " + user_name + " !"
