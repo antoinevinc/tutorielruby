c928cf913a998f1dccecc3275e5964f8071f7fc5
antoinevinc <44704438+antoinevinc@users.noreply.github.com>
2018-11-22 14:01:35 -0400

    Create exo_06.rb

---
+++ exo_06.rb
@@ -0,0 +1,5 @@
+number_of_hours_worked_per_day = 10 # attribut la valeur 10 à la variable number_of_hours_worked_per_day
+number_of_days_worked_per_week = 5 # attribut la valeur 5 à la variable number_of_days_worked_per_week
+number_of_weeks_in_THP = 11 # attribut la valeur 11 à la variable number_of_weeks_in_THP
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
